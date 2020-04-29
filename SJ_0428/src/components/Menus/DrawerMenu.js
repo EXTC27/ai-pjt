bcfd0d9f1d0c1c9a81bb1fd5638e2376d00851d0
Dolphago <adamdoha@users.noreply.github.com>
2020-04-29 16:16:20 +0900

    filter & adjust done

--- SJ_0428/src/components/Menus/DrawerMenu.js
+++ SJ_0428/src/components/Menus/DrawerMenu.js
@@ -1,48 +1,52 @@
-import React, { Component } from 'react';
-import {Storage} from '../Storage';
-import Loading from '../Loading'
-import Adjust from '../Settings/Adjust/Adjust'
-import SegmentList from '../Settings/Segment/SegmentList'
-import FaceList from '../Settings/Face/FaceList'
-import TagList from '../Settings/Tag/TagList'
+import React, { Component } from "react";
+import { Storage } from "../Storage";
+import Loading from "../Loading";
+import Adjust from "../Settings/Adjust/Adjust";
+import SegmentList from "../Settings/Segment/SegmentList";
+import FaceList from "../Settings/Face/FaceList";
+import TagList from "../Settings/Tag/TagList";
 
-import styled from 'styled-components';
-import {IconButton, Button} from '@material-ui/core'
-import {Clear, Check, Refresh} from '@material-ui/icons';
-import RotateMenu from '../Settings/Rotate/RotateMenu'
+import styled from "styled-components";
+import { IconButton, Button } from "@material-ui/core";
+import { Clear, Check, Refresh } from "@material-ui/icons";
+import RotateMenu from "../Settings/Rotate/RotateMenu";
 
-class DrawerMenu extends Component{
-  constructor(props){
+class DrawerMenu extends Component {
+  constructor(props) {
     super(props);
     this.clickTag = this.clickTag.bind(this);
     this.clickCopy = this.clickCopy.bind(this);
   }
 
-  clickTag(key, original, custom, event){
+  clickTag(key, original, custom, event) {
     key = key.i;
 
-    // 태그가 custom에 있는지 확인  
-    if(custom.indexOf(key) === -1){ //없으면 추가
-      custom.push(key)
-      event.currentTarget.className = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeSmall MuiButton-sizeSmall"
-    }else{  //있으면 삭제
-      custom.splice(custom.indexOf(key), 1)
-      event.currentTarget.className = "MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedPrimary MuiButton-outlinedSizeSmall MuiButton-sizeSmall"
+    // 태그가 custom에 있는지 확인
+    if (custom.indexOf(key) === -1) {
+      //없으면 추가
+      custom.push(key);
+      event.currentTarget.className =
+        "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeSmall MuiButton-sizeSmall";
+    } else {
+      //있으면 삭제
+      custom.splice(custom.indexOf(key), 1);
+      event.currentTarget.className =
+        "MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedPrimary MuiButton-outlinedSizeSmall MuiButton-sizeSmall";
     }
   }
 
-  clickCopy(custom, original){
+  clickCopy(custom, original) {
     // index 오름차순 정렬
-    var tmpList = custom.sort(function(a, b){
-      return a - b ;
-    }) ;
-    
+    var tmpList = custom.sort(function (a, b) {
+      return a - b;
+    });
+
     // 텍스트 추출
-    var tags = '';
-    for(var i = 0; i < tmpList.length; i++){
+    var tags = "";
+    for (var i = 0; i < tmpList.length; i++) {
       tags += "#" + original[tmpList[i]] + " ";
     }
-    console.log('복사할 텍스트 ', tags)
+    console.log("복사할 텍스트 ", tags);
 
     // 복사
     var dummy = document.createElement("textarea");
@@ -52,103 +56,109 @@ class DrawerMenu extends Component{
     document.execCommand("copy");
     document.body.removeChild(dummy);
   }
-  
-  render(){
-    var originalTagList = []
-    var customTagList = []
 
-    return(
+  render() {
+    var originalTagList = [];
+    var customTagList = [];
+
+    return (
       <Storage.Consumer>
-      {
-        store => (
+        {(store) => (
           <StDrawerMenuCont id="drawer-container">
             <StMenuCont>
-            {
-              store.curMode === 'tag' ?
-              <IconButton id={store.curMode} onClick={store.applyChange}><Refresh fontSize="large" /></IconButton>
-              :
-              <IconButton id={store.curMode} onClick={store.applyChange}><Check fontSize="large" /></IconButton>
-            }
-              <IconButton id={store.curMode} onClick={store.cancelChange}><Clear fontSize="large"/></IconButton>
+              {store.curMode === "tag" ? (
+                <IconButton id={store.curMode} onClick={store.applyChange}>
+                  <Refresh fontSize="large" />
+                </IconButton>
+              ) : (
+                <IconButton id={store.curMode} onClick={store.applyChange}>
+                  <Check fontSize="large" />
+                </IconButton>
+              )}
+              <IconButton id={store.curMode} onClick={store.cancelChange}>
+                <Clear fontSize="large" />
+              </IconButton>
             </StMenuCont>
 
             <StSettingCont>
+              {
+                store.curMode === "rotate" && <RotateMenu />
+                //<div>
+                //  회전 부분입니다.
+                //</div>
+              }
+              {store.curMode === "filter" && <div>필터 부분입니다.</div>}
+              {store.curMode === "adjust" && <Adjust store={store} />}
+              {store.curMode === "segment" && ( //객체찾기 부분입니다.
+                <>
+                  {store.loading ? <Loading /> : null}
+                  {store.loading ? null : <SegmentList store={store} />}
+                </>
+              )}
 
-            {store.curMode === 'rotate' &&
-              <RotateMenu />
-              //<div>
-              //  회전 부분입니다.
-              //</div>
-            }
-            {store.curMode === 'filter' &&
-              <div>
-                필터 부분입니다.
-              </div>
-            }
-            {store.curMode === 'adjust' &&
-              // <Adjust/>
-              <>
-              <input id="blur" type="range" min="0" max="50" step="10"
-              value={store.blur}
-              onChange={store.changeAdjust}/>
+              {store.curMode === "face" && ( //얼굴찾기 부분입니다.
+                <>
+                  {store.loading ? <Loading /> : null}
+                  {store.loading ? null : <FaceList store={store} />}
+                </>
+              )}
 
-              <input id="hue" type="range" min="-180" max="180" step="1"
-              value={store.hue}
-              onChange={store.changeAdjust}/>
-
-              <input id="lum" type="range" min="-1" max="1" step="0.1"
-              value={store.lum}
-              onChange={store.changeAdjust}/>
-              </>
-            }
-
-            {store.curMode === 'segment' && //객체찾기 부분입니다.
-              <>
-              {store.loading ? <Loading/>:null}
-              {store.loading ? null:<SegmentList store={store}/>}
-              </>
-            }
-
-            {store.curMode === 'face' && //얼굴찾기 부분입니다.
-              <>
-              {store.loading ? <Loading/>:null}
-              {store.loading ? null:<FaceList store={store}/>}
-              </>
-            }
-
-            {store.curMode === 'tag' &&
+              {store.curMode === "tag" && (
                 // <TagList store={store}/>
-              <div>
-                [ 추천 해시태그 ] <br/>
-                {store.tagList.map((tag, i) => {
-                  originalTagList = store.tagList
-                  customTagList.push({i}.i)
-                  return ( 
-                  <Button size="small" variant="contained" color="primary" 
-                    style={{borderRadius:"12px", marginLeft:"4px", marginBottom:"3px"}}
-                    onClick={(e) => this.clickTag({i}, originalTagList, customTagList, e)} key={i}>
-                    {tag}
-                  </Button>);
-                })}
-                <br/>
-                <Button variant="contained" color="primary" href="#contained-buttons" 
-                  style={{position:'fixed', bottom:'10px', right:'10px'}} 
-                  onClick={() =>this.clickCopy(customTagList, originalTagList)}>
-                  Copy
-                </Button>
-              </div>
-            }
+                <div>
+                  [ 추천 해시태그 ] <br />
+                  {store.tagList.map((tag, i) => {
+                    originalTagList = store.tagList;
+                    customTagList.push({ i }.i);
+                    return (
+                      <Button
+                        size="small"
+                        variant="contained"
+                        color="primary"
+                        style={{
+                          borderRadius: "12px",
+                          marginLeft: "4px",
+                          marginBottom: "3px",
+                        }}
+                        onClick={(e) =>
+                          this.clickTag(
+                            { i },
+                            originalTagList,
+                            customTagList,
+                            e
+                          )
+                        }
+                        key={i}
+                      >
+                        {tag}
+                      </Button>
+                    );
+                  })}
+                  <br />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    href="#contained-buttons"
+                    style={{ position: "fixed", bottom: "10px", right: "10px" }}
+                    onClick={() =>
+                      this.clickCopy(customTagList, originalTagList)
+                    }
+                  >
+                    Copy
+                  </Button>
+                </div>
+              )}
             </StSettingCont>
           </StDrawerMenuCont>
-        )
-      }
+        )}
       </Storage.Consumer>
-    )
+    );
   }
-} export default DrawerMenu;
+}
+export default DrawerMenu;
 
 const StDrawerMenuCont = styled.div`
-  font-family: 'Noto Sans KR', sans-serif;
+  font-family: "Noto Sans KR", sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -156,7 +166,7 @@ const StDrawerMenuCont = styled.div`
   /* background-image: url("MainBackground.jpg");
   background-size: cover; */
   /* background: linear-gradient(to right, #66ffff 22%, #ff99cc 100%); */
-  background-color: rgb(64,64,64);
+  background-color: rgb(64, 64, 64);
   border-top: 1px solid gray;
   border-radius: 8px 8px 0 0;
   width: 100%;
@@ -169,19 +179,18 @@ const StDrawerMenuCont = styled.div`
 const StMenuCont = styled.div`
   display: flex;
   align-self: flex-end;
-  
-  .MuiTouchRipple-root{
-    color: white;
-  }
-  svg{
-    color: white;
-  }
 
+  .MuiTouchRipple-root {
+    color: white;
+  }
+  svg {
+    color: white;
+  }
 `;
 
 const StSettingCont = styled.div`
   box-sizing: border-box;
-  color : white;
+  color: white;
   /* border: 3px solid gray; */
   display: flex;
   flex-direction: column;
--- SJ_0428/src/components/Settings/Adjust/Adjust.js
+++ SJ_0428/src/components/Settings/Adjust/Adjust.js
@@ -1,41 +1,139 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import {Button} from '@material-ui/core'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
 
 class Adjust extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      mode: 'rgb',
-
-    }
+      mode: "rgb",
+      hue: 0,
+      saturation: 0,
+      luminance: 0,
+      blur: 0,
+    };
   }
-
+  componentDidMount() {
+    const { store } = this.props;
+    this.setState({
+      hue: store.filterHistory[store.historyIdx].hue,
+      saturation: store.filterHistory[store.historyIdx].saturation,
+      luminance: store.filterHistory[store.historyIdx].luminance,
+      blur: store.filterHistory[store.historyIdx].blur,
+    });
+  }
   tabMode = (e) => {
-    const _mode = e.currentTarget.id
+    const _mode = e.currentTarget.id;
     // console.log(_mode)
     this.setState({
       mode: _mode,
-    })
-  }
+    });
+  };
+
+  onChangeHue = (e) => {
+    this.setState({ hue: e.target.value });
+    this.props.store.changeAdjust(e);
+  };
+  onChangeBlur = (e) => {
+    this.setState({ blur: e.target.value });
+    this.props.store.changeAdjust(e);
+  };
+  onChangeLuminance = (e) => {
+    this.setState({ luminance: e.target.value });
+    this.props.store.changeAdjust(e);
+  };
+  onChangeSaturation = (e) => {
+    this.setState({ saturation: e.target.value });
+    this.props.store.changeAdjust(e);
+  };
 
   render() {
-    return(
+    const { mode, hue, saturation, luminance, blur } = this.state;
+    return (
       <StAdjustCont>
         <StBtnCont>
-          <Button id='hue' variant={this.state.mode === 'hue' && 'outlined'} onClick={this.tabMode}>색상</Button>
-          <Button id='saturation' variant={this.state.mode === 'saturation' && 'outlined'} onClick={this.tabMode}>채도</Button>
-          <Button id='luminance' variant={this.state.mode === 'luminance' && 'outlined'} onClick={this.tabMode}>명도</Button>
-          <Button id='blur' variant={this.state.mode === 'blur' && 'outlined'} onClick={this.tabMode}>블러</Button>
+          <Button
+            id="hue"
+            variant={mode === "hue" && "outlined"}
+            onClick={this.tabMode}
+          >
+            색상
+          </Button>
+          <Button
+            id="saturation"
+            variant={mode === "saturation" && "outlined"}
+            onClick={this.tabMode}
+          >
+            채도
+          </Button>
+          <Button
+            id="luminance"
+            variant={mode === "luminance" && "outlined"}
+            onClick={this.tabMode}
+          >
+            명도
+          </Button>
+          <Button
+            id="blur"
+            variant={mode === "blur" && "outlined"}
+            onClick={this.tabMode}
+          >
+            블러
+          </Button>
         </StBtnCont>
 
         <StSliderCont>
+          {mode === "hue" && (
+            <input
+              id="hue"
+              type="range"
+              min="0"
+              max="300"
+              step="0.1"
+              value={hue}
+              onChange={this.onChangeHue}
+            />
+          )}
+          {mode === "saturation" && (
+            <input
+              id="saturation"
+              type="range"
+              min="-5"
+              max="5"
+              step="0.01"
+              value={saturation}
+              onChange={this.onChangeSaturation}
+            />
+          )}
+          {mode === "luminance" && (
+            <input
+              id="lum"
+              type="range"
+              min="-1"
+              max="1"
+              step="0.01"
+              value={luminance}
+              onChange={this.onChangeLuminance}
+            />
+          )}
 
+          {mode === "blur" && (
+            <input
+              id="blur"
+              type="range"
+              min="0"
+              max="50"
+              step="10"
+              value={blur}
+              onChange={this.onChangeBlur}
+            />
+          )}
         </StSliderCont>
       </StAdjustCont>
-    )
+    );
   }
-} export default Adjust;
+}
+export default Adjust;
 
 const StAdjustCont = styled.div`
   display: flex;
@@ -49,17 +147,16 @@ const StAdjustCont = styled.div`
 const StBtnCont = styled.div`
   display: flex;
 
-  .MuiButton-root{
+  .MuiButton-root {
   }
-  .MuiButton-outlined{
+  .MuiButton-outlined {
     border-bottom-color: rgba(0, 0, 0, 0);
   }
-
 `;
 
 const StSliderCont = styled.div`
   /* border: 1px solid rgba(0, 0, 0, 0); */
   border: 1px solid rgba(0, 0, 0, 0.23);
   width: 100%;
-  height: 100%
+  height: 100%;
 `;
